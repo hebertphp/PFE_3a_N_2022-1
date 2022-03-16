d39176b69ec04a1df2cbc6d4080bb864479791b9
Hebert <hebertphp@gmail.com>
2022-03-15 21:46:10 -0300

    PFE_3a_N

--- aula3/script3_n1.js
+++ aula3/script3_n1.js
@@ -1,18 +1,21 @@
-var dia, nota, ano, mes;
+var dia, nota, ano, mes, nome;
 dia=15;
 nota=9.8;
 ano=2022;
 mes="março";
+nome="Hebert";
 document.write(dia);//15
 document.write("<br>");
 document.write(nota);//9.8
 document.write("<hr>");
 document.write(dia + "/" + mes + "/"+ano);
 document.write("<hr>");
-
+document.write("Hebert, tirei nota: "+nota);
+document.write("<hr>");
 /*
 Saída:
 15/março/2022
+Hebert, tirei nota: 9.8
 
 
 */
