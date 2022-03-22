72e20884f10c2395a90d3ffba7b7b8460fbd67d0
Hebert <hebertphp@gmail.com>
2022-03-22 20:45:06 -0300

    PFE_3a_N

---
+++ aula3/script3_05-1.js
@@ -0,0 +1,22 @@
+const pi = 3.14;//declarar a const pi=3.14
+const terca_feira = "Front-End";
+//pi = 5;//
+var x , y;//
+y = 3;
+y = 2;
+x = y * pi;//x = 2 * 3.14 //x=6.28
+document.write ("x = "+ x);//x = 6.28
+document.write("<br>"+terca_feira);
+/*
+Saída: 
+x = 
+
+
+var z;
+z=10;
+z=20;
+z=30;
+document.write("z="+z);
+z=40;
+z=0;
+*/
