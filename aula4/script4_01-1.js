660b710d29af9ca3644e9b775bb0a60dda300838
Hebert <hebertphp@gmail.com>
2022-03-29 20:54:11 -0300

    PFE_3a_N

---
+++ aula4/script4_01-1.js
@@ -0,0 +1,9 @@
+var x, y ,z;
+y=3;
+z=5;
+x=2*y+z;
+document.write ("x = "+x);//x = 11
+/*
+parseInt   - inteiros
+parseFloat - fracionados(float)
+*/
---
+++ aula4/script4_02-1.js
@@ -0,0 +1,25 @@
+var a,b;//
+a=prompt("Digite um número");//17
+a=parseInt(a);//
+b=prompt("Digite outro número");//2
+b=parseInt(b);//
+document.write ("soma: "+ (a+b));
+document.write ("<br>subtração: "+ (a-b));
+document.write ("<br>multiplicação: "+ (a*b));
+document.write("<br>divisão: " + (a / b));
+document.write("<br>modulo: " + (a % b));
+document.write("<br>exponenciação: " + (a ** b));
+/*
+Desafio:
+Completar o código com os outros 
+operadores aritméticos.
+
+5*2=10
+5**2=5*5=25
+resto da div somente com num int
+17%2= todo número ímpar resto da div por 2, vale 1
+18%2= todo número par resto da div   por 2, vale 0
+
+7%4=3
+5%3=2
+*/
