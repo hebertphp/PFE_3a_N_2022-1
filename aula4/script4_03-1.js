dc0cbe44d8fa456f587257525bfbdf18a71a3555
Hebert <hebertphp@gmail.com>
2022-03-29 21:26:49 -0300

    PFE_3a_N

---
+++ aula4/script4_03-1.js
@@ -0,0 +1,16 @@
+var a,b;
+a=prompt("Digite um número");//6
+a=parseInt(a);
+b=prompt("Digite outro número");//3
+b=parseInt(b);
+b+=a;//b=9
+a*=5;//a=30
+document.write ("a = " + a);//
+document.write ("<hr>b = "+ b);//
+/*
+Comandos de saída:
+no documento(html) quero escrever algo
+document.write( );
+
+
+*/
