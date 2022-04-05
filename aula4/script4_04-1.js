330073971b984748181e6f8b8102334707ec08c5
Hebert <hebertphp@gmail.com>
2022-04-05 19:21:12 -0300

    PFE_3a_N

--- aula4/script4_04-1.js
+++ aula4/script4_04-1.js
@@ -1,7 +1,10 @@
 var a,b;
-a=prompt("Digite o nome");//Astrogildo
-b=prompt("Digite o sobrenome");//Pafuncio
-document.write (a +" "+ b);//
-b+=" ";
-b+=a;
+a=prompt("Digite o nome");//"Astrogildo"
+b=prompt("Digite o sobrenome");//"Pafuncio"
+document.write (a+" "+b);//"Astrogildo Pafuncio"
+b+=" ";//"Pafuncio "
+b+=a;//Pafuncio Astrogildo
 document.write ("<br>"+ b);//
+/*
+
+*/
---
+++ aula4/script4_05-1.js
@@ -0,0 +1,5 @@
+var a;
+a=prompt("Digite um número");//7
+a=parseInt(a);
+document.write ("a ="+ (++a));//6
+document.write ("<br>a ="+ a);
