f709ce56dd084cb7769e5fa369363cb7a7a1d4dc
Hebert <hebertphp@gmail.com>
2022-04-05 20:31:31 -0300

    PFE_3a_N

--- aula4/script4_05-1.js
+++ aula4/script4_05-1.js
@@ -1,5 +1,19 @@
 var a;
-a=prompt("Digite um número");//7
-a=parseInt(a);
-document.write ("a ="+ (++a));//6
-document.write ("<br>a ="+ a);
+a=prompt("Digite um número");//"2022"
+a=parseInt(a);//2022
+document.write ("a ="+ (a--));//2022
+document.write ("<br>a ="+ a);//2021
+/*
+++ incremento somar    1
+-- decremento subtrair 1
+
+++a pré-incremento
+a++ pós-incremento
+--a pré-decremento
+a-- pós-decrmento
+
+
+Saída:
+a =
+a =
+*/
