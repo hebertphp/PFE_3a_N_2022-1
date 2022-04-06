24da8ae1c27564298f7932ec7950db872cea5a1e
Hebert <hebertphp@gmail.com>
2022-04-05 22:04:06 -0300

    PFE_3a_N

--- aula4/script4_15-1.js
+++ aula4/script4_15-1.js
@@ -1,6 +1,12 @@
 //Operador ! (not)
 var a,b,res;
-a=9;//9
-b=10;//10
-res=!(a<b);//res=!( V ) //res=false
+a=10;//10
+b=9;//9
+res=!(a<b);//res=!( F ) //res=true
 document.write("res = "+res);
+/*
+! não, inverte o resultado
+        da condição
+!(V)=F        
+!(F)=V
+*/
---
+++ aula4/script4_18-1.js
@@ -0,0 +1,8 @@
+var a,b,c,d,e;
+a=50;
+b=120;
+c=200;
+d=(a<=b) ? "blz" : "so que não";//d=blz
+e=(a>=c) ? "ok" : "zoado";//e=zoado
+document.write("d = "+d);//d = blz
+document.write("<br>e = "+e);//e = zoado
