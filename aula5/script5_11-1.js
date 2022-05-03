65e544bdc198b229cf10fc8d499d1b3f657b493e
Hebert <hebertphp@gmail.com>
2022-05-03 20:16:14 -0300

    PFE_3a_N

--- aula5/script5_11-1.js
+++ aula5/script5_11-1.js
@@ -1,20 +1,19 @@
 var i,j;
-for (i=0; i<3; i++) {//ext //hora
- for (j=0; j<4; j++) {//int //min
-   document.write(i+" "+j+"<br>");
- }
+//   valor inicial;condição;contador
+for (i=1; i<4; i++) {//ext //hora
+  for (j=0; j<3; j++) {//int //min
+    document.write(i+" "+j+"<br>");
+  }
 }
 /*
-0 0
-0 1
-0 2
-0 3
-1 0
-1 1
-1 2
-1 3
-2 0
-2 1
-2 2
-2 3
+i   j
+1   0
+1   1
+1   2 
+2   0
+2   1
+2   2
+3   0
+3   1
+3   2
 */
