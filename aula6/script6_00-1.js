8f5c00e70f9b42db6235c312eff641c5614b2e54
Hebert <hebertphp@gmail.com>
2022-05-03 21:08:13 -0300

    PFE_3a_N

--- aula6/script6_00-1.js
+++ aula6/script6_00-1.js
@@ -1,11 +1,17 @@
-var num,x;
+var num,x,msg;
 num=5;//5
 num=78;//78
 num=2021;//2021
 num=8;//8
 num=71;//71
 num=130;//130
-document.write(num);//130
+//document.write(num);//130
+//document.getElementById("dv1").innerHTML="num ="+num;
+document.querySelector("#dv1").innerHTML="<b>num</b> = "+num;
 
 x=[5,78,2021,8,71,130];
-document.write("<br>"+x);
+//document.write("<br>"+x);
+document.querySelector(".teste").innerHTML=x;
+
+//msg=", Aula 6";
+//document.querySelector("h1").innerHTML=msg;
