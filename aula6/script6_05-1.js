8928634f43423ef2a3285e00007803362a035f96
Hebert <hebertphp@gmail.com>
2022-05-10 21:59:49 -0300

    PFE_3a_N

--- aula6/script6_05-1.js
+++ aula6/script6_05-1.js
@@ -1,8 +1,9 @@
 var num=[10,20,30,40,50];
 var tam;
 tam=num.length;
-
-
+document.querySelector("#dv1").innerHTML="var num possui "+tam+" valores<br>"+"Todos os valores da var num: "+num+"<hr>";
+document.querySelector(".teste").innerHTML="Valor da primeira posição de num: "+num[0]+"<hr>"+"Valor da última posição de num: "+num[tam-1]+"<hr>";
+document.querySelector("h2").innerHTML=num;
 /*
 document.write("var num possui "+tam+" valores<hr>");
 document.write("Todos os valores da var num: "+num+"<hr>");
