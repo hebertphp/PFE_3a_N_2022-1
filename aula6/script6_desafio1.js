e804b9a9f669615cb8319722936c101166436f83
Hebert <hebertphp@gmail.com>
2022-05-17 21:14:27 -0300

    PFE_3a_N

--- aula6/script6_desafio1.js
+++ aula6/script6_desafio1.js
@@ -1,3 +1,8 @@
+for(var i=9193;i<9211;i++){
+    document.querySelector("#dv1").innerHTML+="&#"+i+" ";
+}
+//document.querySelector("#dv1").innerHTML="&#9193";
+
 /*
 for(valor_inicial;condição;contador)
 for(var i=9193;i<9211;i++)
@@ -6,12 +11,4 @@ Desafio1:
 Criar um looping de:9193 até:9210
 A saída mostrasse os emojis dos valores da faixa acima
 link: https://www.w3schools.com/charsets/ref_emoji.asp
-
-
-
-
-Desafio2:
-De: 9800 até:9811
-Mostrar o Emoji e o signo("string")
-Criar um array com os signos
 */
---
+++ aula6/script6_desafio2.js
@@ -0,0 +1,6 @@
+/*
+Desafio2:
+De: 9800 até:9811
+Mostrar o Emoji e o signo("string")
+Criar um array com os signos
+*/
