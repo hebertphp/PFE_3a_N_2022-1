d15c462f38dce2c92913790e318e604188ee1b42
Hebert <hebertphp@gmail.com>
2022-05-24 20:41:26 -0300

    PFE_3a_N

--- aula6/duvida.js
+++ aula6/duvida.js
@@ -1,5 +1,11 @@
-var uf=["SP","RJ","MG","ES","TO"]
-for(var i=0;i<5;i++){
+var uf=["SP","RJ","MG","ES","TO","PA","PR","AM","BA","PB"];
+var txt="Hoje tem atividade!";
+var class_teste=document.querySelectorAll(".teste");
+for(var i=0;i<uf.length;i++){
     document.querySelector("#dv1").innerHTML+=uf[i]+"<br>";
 }
-document.querySelector("h2").innerHTML+=", "+uf;
+document.querySelector("h2").innerHTML+=", "+uf.length;
+// document.querySelector(".teste").innerHTML=txt;
+class_teste[3].innerHTML=txt;
+document.querySelector("h2").style.color="blue";
+class_teste[1].style.backgroundColor="blue";
