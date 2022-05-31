91292c4d6b055c4fc08a13d3726436b988342c2c
Hebert <hebertphp@gmail.com>
2022-05-31 20:25:41 -0300

    PFE_3a_N

--- aula6/script6_06.js
+++ aula6/script6_06.js
@@ -1,6 +1,6 @@
-var frutas = ["Laranja", "Morango", "Melão", "Figo"];
+var frutas = ["Laranja", "Morango", "Melão", "Figo",7.5,2022];
 var junto = frutas.toString();
 var junta_mesmo = frutas[0]+","+frutas[1]+",";
 // document.write(junto);
-document.querySelector("#dv").innerHTML=frutas[0];
+document.querySelector("#dv").innerHTML=junto;
 // document.querySelector(".teste").innerHTML=junto;
