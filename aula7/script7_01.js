42382b3cabe292657e31e8376cf77b48df3719f5
Hebert <hebertphp@gmail.com>
2022-06-14 20:38:53 -0300

    PFE_3a_N

--- aula7/script7_01.js
+++ aula7/script7_01.js
@@ -4,7 +4,18 @@ Objetivo: Enviar mensagem de boas vindas
 Data de criação: 16/05/2021
 Autor: Nome do autor
 -------------------------------------------- */
-function boas_vindas() {
-    document.write("Seja bem vindo!");
+function boas_vindas(pessoa) {
+    document.write("Seja bem vindo(a): "+pessoa+"!");
+    document.write("<hr>");
+}//criação, construir, declarar
+function negrito(txt){
+    document.write("<b>"+txt+"</b>");
 }
-boas_vindas();
+function titulo2(tit){
+    document.write("<h2>"+tit+"</h2>");
+}
+
+titulo2("Título 2!!");
+boas_vindas("Jubileu");//execução, chamada da função
+negrito("Este texto está em negrito");
+titulo2("Último título 2");
