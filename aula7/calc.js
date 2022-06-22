3eee8b3eb16294ebb278fd1a3572846002a81ae8
Hebert <hebertphp@gmail.com>
2022-06-21 21:14:50 -0300

    PFE_3a_N

--- aula7/calc.js
+++ aula7/calc.js
@@ -4,7 +4,13 @@ function mostra(){
     const id_n2=document.querySelector("#n2");
     const id_op=document.querySelector("#op");
     
-    id_saida.innerHTML=id_n1.value+"<br>"+id_op.value+"<br>"+id_n2.value;
+    id_saida.innerHTML=id_n1.value+"<br>"+id_op.value+"<br>"+id_n2.value+"<br>------<br>";
+    id_saida.innerHTML+=calc(parseFloat(id_n1.value),parseFloat(id_n2.value),id_op.value);
+    //id_saida.innerHTML+=calc(parseInt(id_n1),parseInt(id_n2),id_op);
+    //id_saida.innerHTML+=calc(id_n1.value,id_n2.value,id_op.value);
+   // parseInt("7.8")//7
+   // parseFloat("7.8")//7.8
+    //calc(3,5,"*")//15
 }
 
 function calc(n1,n2,op){
