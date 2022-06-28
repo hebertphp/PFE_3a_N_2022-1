b3150dc172cfc6fdf7749e3c8920cf24562565d6
Hebert <hebertphp@gmail.com>
2022-06-28 20:23:21 -0300

    PFE_3a_N

--- aula7/script7_06.js
+++ aula7/script7_06.js
@@ -1,14 +1,14 @@
+var x = 298;//escopo global
 function par_impar() {
-    var x = 29;//escopo local
-    document.write(x+"--");
+    document.write(x+"---");
     if (x % 2 == 0) {
         var msg = " PAR<br>";//escopo local/bloco
     }
     else {
         var msg = " ÍMPAR<br>";//escopo local/bloco
     }
-    document.write(msg+"");
+    document.write("=="+msg);
 }
-
 par_impar();
-document.write(x);//não imprime o valor de x
+document.write(x+"???");
+document.write(msg);//não imprime o valor da var
